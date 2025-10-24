3f7a94e45c0f0c5f07cd31ff9d9d44c7700a7a71
roman <sandakovroman2@gmail.com>
2025-10-24 12:36:31 +0300

    As an app developer I want to get a list of all games to show in my app #429532

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json');
 
+app.get('/games', (req, res) => {
+    res.send(["Witcher 3", "Cyberpunk 2077", "Minecraft"])
+})
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
