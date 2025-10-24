33001ebfdf42055d26f2dc90a6e59d6892defa78
roman <sandakovroman2@gmail.com>
2025-10-24 14:05:48 +0300

    kõigi võimalike elementide väljajätmine nimekirjast

--- index.js
+++ index.js
@@ -3,8 +3,17 @@ const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json');
 
+const games = [
+    {id: 1, name: "Witcher 3", price: 29.99},
+    {id: 2, name: "Cyberpunk 2077", price: 59.99},
+    {id: 3, name: "Minecraft", price: 26.99}
+]
 app.get('/games', (req, res) => {
-    res.send(["Witcher 3", "Cyberpunk 2077", "Minecraft"])
+    res.send(games)
+})
+
+app.get('/games/:id', (req, res) => {
+    res.send(games[req.params.id - 1])
 })
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
