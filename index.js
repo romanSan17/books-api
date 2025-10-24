05e2c2e9036cc1feb96889561eb7be65ad6f095e
roman <sandakovroman2@gmail.com>
2025-10-24 15:47:16 +0300

    add books

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const yamljs = require('yamljs')
 const swaggerDocument = yamljs.load('./docs/swagger.yaml')
+
+app.use(express.json())
 
 const games = [
     {id: 1, name: "Witcher 3", price: 29.99},
@@ -22,8 +25,30 @@ app.get('/games/:id', (req, res) => {
     res.send(games[req.params.id - 1])
 })
 
+app.post('/games', (req, res) => {
+    if (!req.body.name || !req.body.price) {
+        return res.status(400).send({error: 'One or all params are missing'})
+    }
+    let game = {
+        id: games.length + 1,
+        price: req.body.price,
+        name: req.body.name
+    }
+    game.push(game)
+
+    res.status(201)
+        .location(`${getBaseUrl(req)}/games/${games.length}`)
+        .send(game)
+})
+
+
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.listen(port, () => {
     console.log(`API up at: http://localhost:${port}`)
 })
+
+function getBaseUrl(req) {
+    return req.connection && req.connection.encrypted
+        ? 'https' : 'http' + `://{req.headers.host}`
+}
